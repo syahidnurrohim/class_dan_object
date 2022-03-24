34768308df1a953324a7bdd6a0b7a221a668cdfa
syahidnurrohim <syahidnurrohim@gmail.com>
2022-03-25 05:34:15 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDanObject
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Taxi taxi = new Taxi();
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPasengger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDanObject
+{
+    internal class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", this.DriverName);
+            Console.WriteLine("On Duty: {0}", this.OnDuty ? "Yes" : "No");
+            Console.WriteLine("Number of Passenger: {0}", this.NumPassenger);
+            Console.WriteLine();
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("Jono sedang menjemput penumpang");
+        }
+        public void DropOffPasengger()
+        {
+            Console.WriteLine("Jono sedang mengantar penumpang");
+        }
+    }
+}
